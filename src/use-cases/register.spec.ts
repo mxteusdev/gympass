cc3f029c67c2ba244b9c5e611a6cfcab3ff320c5
Mateus Rodrigues <mateusjaed@gmail.com>
2024-06-03 18:29:41 +0000

    added func, routes and controller of authenticate

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -4,8 +4,6 @@ import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
-// test session
-
 describe('Register Use Case', () => {
   it('should be able to register', async () => {
     const usersRepository = new InMemoryUsersRepository()
