dd005ce29cc9f81b12f3024b1ce401f30a4a7171
Mateus Rodrigues <mateusjaed@gmail.com>
2024-06-10 18:07:20 +0000

    added errors, and done func create gym use case/ gym spec tests

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -3,24 +3,26 @@ import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-c
 import { CheckInUseCase } from './check-in'
 import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repositories'
 import { Decimal } from '@prisma/client/runtime/library'
+import { MaxDistanceError } from './errors/max-distance-error'
+import { MaxNumberOfCheckInsError } from './errors/max-number-of-check-ins-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let gymsRepository: InMemoryGymsRepository
 let sut: CheckInUseCase
 
 describe('Check-in Use Case', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository()
     gymsRepository = new InMemoryGymsRepository()
     sut = new CheckInUseCase(checkInsRepository, gymsRepository)
 
-    gymsRepository.items.push({
+    await gymsRepository.create({
       id: 'gym-01',
       title: 'JavaScript Gym',
       description: '',
       phone: '',
-      latitude: new Decimal(-23.5506),
-      longitude: new Decimal(-46.6333),
+      latitude: -23.5506,
+      longitude: -46.6333,
     })
 
     vi.useFakeTimers()
@@ -58,7 +60,7 @@ describe('Check-in Use Case', () => {
         userLatitude: -23.5506,
         userLongitude: -46.6333,
       }),
-    ).rejects.toBeInstanceOf(Error)
+    ).rejects.toBeInstanceOf(MaxNumberOfCheckInsError)
   })
 
   it('should be able to check in twice but in different days', async () => {
@@ -100,6 +102,6 @@ describe('Check-in Use Case', () => {
         userLatitude: -23.5506,
         userLongitude: -46.6333,
       }),
-    ).rejects.toBeInstanceOf(Error)
+    ).rejects.toBeInstanceOf(MaxDistanceError)
   })
 })
---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repositories'
+import { expect, describe, it, beforeEach } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'JavaScript Gym',
+      description: null,
+      phone: null,
+      latitude: -23.5489,
+      longitude: -46.6388,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
