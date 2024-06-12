ce6019362df3b7f698b09683cc568f9e484392dc
Mateus Rodrigues <mateusjaed@gmail.com>
2024-06-12 16:56:59 +0000

    feature get user metrics, recive historys of check-ins on dashboard

---
+++ src/use-cases/search-gyms.ts
@@ -0,0 +1,32 @@
+import { Gym } from '@prisma/client'
+import { GymsRepository } from '@/repositories/gyms.repository'
+
+interface SearchGymsUseCaseRequest {
+  query: string
+  page: number
+}
+
+interface SearchGymsUseCaseResponse {
+  gyms: Gym[]
+}
+
+export class SearchGymsUseCase {
+  constructor(private gymsRepository: GymsRepository) {}
+
+  async execute({
+    query,
+    page,
+  }: SearchGymsUseCaseRequest): Promise<SearchGymsUseCaseResponse> {
+    const gym = await this.gymsRepository.create({
+      title,
+      description,
+      phone,
+      latitude,
+      longitude,
+    })
+
+    return {
+      gym,
+    }
+  }
+}
